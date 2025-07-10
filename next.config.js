d9d38c17e1596a95f8751ee2c998cce3e48bfe16
zyodritesh <ritesh@zyod.com>
2025-07-11 00:34:00 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
+    SHEET_ID: process.env.SHEET_ID,
+  },
+};
